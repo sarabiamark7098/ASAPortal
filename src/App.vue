5f00b5a53cc877df1430e1adf6e5f36f81be0c48
sarabiamark7098 <sarabiamark0012@gmail.com>
2025-03-26 14:34:58 +0800

    trial

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">About</RouterLink>
       </nav>
     </div>
   </header>
